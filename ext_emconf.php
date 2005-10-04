ca658b0dae2fc7ed0ace48cb6fc6c0b9ef3d2df0
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-10-04 22:15:42 +0000

    New version.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dubletfinder/trunk@624 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "dubletfinder"
 # 
-# Auto generated 30-09-2005 04:56
+# Auto generated 05-10-2005 00:15
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -13,25 +13,30 @@ $EM_CONF[$_EXTKEY] = Array (
 	'title' => 'Dublet Finder',
 	'description' => 'This extension finds entries from tt_address and fe_users that share the same e-mail address (dublets/duplicates/double entries) and deletes all but one (for each address), merging the flags in the process. The remaining entry is moved to a different page.',
 	'category' => 'module',
-	'author' => 'Oliver Klee',
-	'author_email' => 'typo3-coding@oliverklee.de',
-	'shy' => '',
+	'shy' => 0,
 	'dependencies' => 'direct_mail',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '-',
+	'PHP_version' => '-',
 	'module' => '',
 	'state' => 'alpha',
-	'internal' => '',
+	'internal' => 0,
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
+	'author' => 'Oliver Klee',
+	'author_email' => 'typo3-coding@oliverklee.de',
 	'author_company' => '',
-	'private' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'private' => 0,
 	'download_password' => '',
-	'version' => '0.0.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:6:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"e791";s:16:"locallang_db.php";s:4:"3ffb";s:8:"todo.txt";s:4:"0c3d";s:43:"modfunc1/class.tx_dubletfinder_modfunc1.php";s:4:"465e";s:22:"modfunc1/locallang.php";s:4:"0a5a";}',
+	'version' => '0.1.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:6:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"e791";s:16:"locallang_db.php";s:4:"3ffb";s:8:"todo.txt";s:4:"0c3d";s:43:"modfunc1/class.tx_dubletfinder_modfunc1.php";s:4:"f78b";s:22:"modfunc1/locallang.php";s:4:"fbdc";}',
 );
 
 ?>
