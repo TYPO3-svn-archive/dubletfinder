10a1aedacd96d124f99f4de34a2ece7572022160
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2005-10-05 16:00:00 +0000

    New version.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dubletfinder/trunk@633 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "dubletfinder"
 # 
-# Auto generated 05-10-2005 00:15
+# Auto generated 05-10-2005 17:59
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -11,7 +11,7 @@
 
 $EM_CONF[$_EXTKEY] = Array (
 	'title' => 'Direct Mail Companion',
-	'description' => 'This extension helps you maintain your Direct Mail subscriber record. It can remove whitespace and quotes around e-mail addresses, delete records with an invalid or empty e-mail address and remove duplicates.',
+	'description' => 'This extension helps you maintain your Direct Mail subscriber records. It can remove whitespace and quotes around e-mail addresses, delete records with an invalid or empty e-mail address and remove duplicates.',
 	'category' => 'module',
 	'shy' => 0,
 	'dependencies' => 'direct_mail',
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = Array (
 	'CGLcompliance_note' => '',
 	'private' => 0,
 	'download_password' => '',
-	'version' => '0.1.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:6:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"e791";s:16:"locallang_db.php";s:4:"3ffb";s:8:"todo.txt";s:4:"0c3d";s:43:"modfunc1/class.tx_dubletfinder_modfunc1.php";s:4:"f78b";s:22:"modfunc1/locallang.php";s:4:"fbdc";}',
+	'version' => '0.2.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:7:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"e791";s:16:"locallang_db.php";s:4:"6e0e";s:8:"todo.txt";s:4:"2fd0";s:14:"doc/manual.sxw";s:4:"dc8c";s:43:"modfunc1/class.tx_dubletfinder_modfunc1.php";s:4:"7d69";s:22:"modfunc1/locallang.php";s:4:"425f";}',
 );
 
 ?>
